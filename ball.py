0ac3a9c4a8c15e87a25bda7c548079aa61e9e0e3
ornichola <1502708+ornichola@users.noreply.github.com>
2019-04-25 23:47:25 +0300

    fix Ball game object creating

--- ball.py
+++ ball.py
@@ -5,7 +5,7 @@ from game_object import GameObject
 
 class Ball(GameObject):
     def __init__(self, x, y, r, color, speed):
-        GameObject.__init__(x-r, y-r, r*2, r*2, speed)
+        GameObject.__init__(self, x-r, y-r, r*2, r*2, speed)
         self.radius = r
         self.diameter = r * 2
         self.color = color
