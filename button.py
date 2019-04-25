2206f8700642cacc2de0ca3bd5589188356e0962
ornichola <1502708+ornichola@users.noreply.github.com>
2019-04-25 14:20:13 +0300

    add init and  methods for button.py::Button

--- button.py
+++ button.py
@@ -1,9 +1,26 @@
 import pygame
 
+import config as c
+
 from game_object import GameObject
+from text_object import TextObject
 
 
 class Button(GameObject):
+    def __init__(self, x, y, w, h, text, on_click=lambda x: None, padding=0):
+        super().__init__(x, y, w, h)
+        self.state = 'normal'
+        self.on_click = on_click
+
+        self.text = TextObject(x + padding, y + padding, lambda: text, c.button_text_color, c.font_name, c.font_size)
+
+    @property
+    def back_color(self):
+        return dict(
+            normal=c.button_normal_back_color,
+            hover=c.button_hover_back_color,
+            pressed=c.button_pressed_back_color
+        )[self.state]
 
     def handle_mouse_event(self, type, pos):
         if type == pygame.MOUSEMOTION:
@@ -28,3 +45,7 @@ class Button(GameObject):
         if self.state == 'pressed':
             self.on_click(self)
             self.state = 'hover'
+
+    def draw(self, surface):
+        pygame.draw.rect(surface, self.back_color, self.bounds)
+        self.text.draw(surface)
--- config.py
+++ config.py
@@ -4,6 +4,14 @@ ball_color = colors.GREEN
 ball_radius = 8
 ball_speed = 3
 
+button_normal_back_color = colors.INDIANRED1
+button_hover_back_color = colors.INDIANRED2
+button_pressed_back_color = colors.INDIANRED3
+button_text_color = colors.WHITE
+
+font_name = 'Arial'
+font_size = 20
+
 screen_width = 600
 screen_height = 800
 
