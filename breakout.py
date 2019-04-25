2882ae606cdbeca462fd00981cabb62405ef688b
ornichola <1502708+ornichola@users.noreply.github.com>
2019-04-25 13:43:53 +0300

    add ball collisions handling for main game class

--- breakout.py
+++ breakout.py
@@ -27,6 +27,79 @@ class Breakout(Game):
         self.ball = Ball(c.screen_width//2, c.screen_height//2, c.ball_radius, c.ball_color, speed)
         self.objects.append(self.ball)
 
+    def handle_ball_collisions(self):
+        def intersect(obj, ball):
+            edges = dict(
+                left=pygame.Rect(obj.left, obj.top, 1, obj.height),
+                right=pygame.Rect(obj.right, obj.top, 1, obj.height),
+                top=pygame.Rect(obj.left, obj.bottom, obj.width, 1),
+                bottom=pygame.Rect(obj.left, obj.bottom, obj.width, 1)
+            )
+            collisions = set(edge for edge in edges.items() if ball.bounds.colliderect(rect))
+            if not collisions:
+                return None
+
+            if len(collisions) == 1:
+                return list(collisions)[0]
+
+            if 'top' in collisions:
+                if ball.centery >= obj.top:
+                    return 'top'
+                if ball.centerx < obj.left:
+                    return 'left'
+                else:
+                    return 'right'
+
+            if 'bottom' in collisions:
+                if ball.centery >= obj.bottom:
+                    return 'bottom'
+                if ball.centerx < obj.left:
+                    return 'left'
+                else:
+                    return 'right'
+
+            s = self.ball.speed
+            edge = intersect(self.paddle, self.ball)
+            if edge is not None:
+                self.sound_effects['paddle_hit'].play()
+            if edge == 'top':
+                speed_x = s[0]
+                speed_y = -s[1]
+                if self.paddle.moving_left:
+                    speed_x -= 1
+                elif self.paddle.moving_left:
+                    speed_x += 1
+                self.ball.speed = speed_x, speed_y
+            elif edge in ('left', 'right'):
+                self.ball.speed = (-s[0], s[1])
+
+            if self.ball.top > c.screen_height:
+                self.lives -= 1
+            if self.lives == 0:
+                self.game_over = True
+            else:
+                self.create_ball()
+
+            if self.ball.top < 0:
+                self.ball.speed(s[0], -s[1])
+
+            if self.ball.left < 0 or self.ball.right > c.screen_width:
+                self.ball.speed = (-s[0], s[1])
+
+            for brick in self.bricks:
+                edge = intersect(brick, self.ball)
+                if not edge:
+                    continue
+
+                self.bricks.remove(brick)
+                self.objects.remove(brick)
+                self.score += self.points_per_brick
+
+                if edge in ('top', 'bottom'):
+                    self.ball.speed = (s[0], -s[1])
+                else:
+                    self.ball.speed = (-s[0], s[1])
+
 
 def main():
     Breakout().run()
