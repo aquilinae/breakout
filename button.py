925d48702d4f48dc32746af18fe3b701e75d830e
ornichola <1502708+ornichola@users.noreply.github.com>
2019-04-25 11:26:45 +0300

    implement initial button.py::Button -> methods for handling mouse events

---
+++ button.py
@@ -0,0 +1,30 @@
+import pygame
+
+from game_object import GameObject
+
+
+class Button(GameObject):
+
+    def handle_mouse_event(self, type, pos):
+        if type == pygame.MOUSEMOTION:
+            self.handle_mouse_move(pos)
+        elif type == pygame.MOUSEBUTTONDOWN:
+            self.handle_mouse_down(pos)
+        elif type == pygame.MOUSEBUTTONUP:
+            self.handle_mouse_up(pos)
+
+    def handle_mouse_move(self, pos):
+        if self.bounds.collidepoint(pos):
+            if self.state != 'pressed':
+                self.state = 'hover'
+        else:
+            self.state = 'normal'
+
+    def handle_mouse_down(self, pos):
+        if self.bounds.collidepoint(pos):
+            self.state = 'pressed'
+
+    def handle_mouse_up(self, pos):
+        if self.state == 'pressed':
+            self.on_click(self)
+            self.state = 'hover'
